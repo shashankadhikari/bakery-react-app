f41256212e68fcf49a657127879a7bd777d60d76
shashank adhikari <shashank@shashanks-MacBook-Air.local>
2022-07-11 22:56:43 +0545

    just use random title and image today

---
+++ src/components/Popular.jsx
@@ -0,0 +1,90 @@
+import { useEffect,useState  } from "react";
+import styled from "styled-components"
+import {Splide, SplideSlide} from "@splidejs/react-splide"
+import '@splidejs/react-splide/css';
+
+function Popular() {
+
+    const [popular, setPopular] = useState([]);
+
+    useEffect(() => {
+      getPopular();
+    }, [])
+    
+
+const getPopular = async () => {
+    const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json(); 
+    setPopular(data.recipes);
+}
+
+  return (
+    <div>
+        <Wrapper>
+            <h3>Popular Picks</h3>
+            <Splide options={{
+                perPage: 4,
+                arrows: false,
+                pagination: false,
+                drag: "free",
+                gap: "5rem"
+            }}>
+            {popular.map((recipe) => {
+                return(
+                    <SplideSlide>
+                    <Card>
+                        <p>{recipe.title}</p>
+                        <img src={recipe.image} alt={recipe.title} />
+                    </Card>
+                    </SplideSlide>
+                )
+            })}
+            </Splide>
+        </Wrapper>
+    </div>
+  ); 
+}
+
+const Wrapper = styled.div`
+    margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+    min-height: 20rem;
+    border-radius: 2rem;
+    overflow: hidden;
+    position: relative;
+
+    img {
+        border-radius:2rem;
+        postion: adsolute
+        left: 0;
+        width: 100%
+        height: 100%
+        object-fit: cover;
+    }
+
+    p {
+        postion: absolute;
+        z-index: 10;
+        left: 50%;
+        bottom: 0%;
+        transfrom: translate(-50%, 0%);
+        color: green;
+        width:100%;
+        text-align:center;
+        font-weight: 600;
+        font-size: 1rem;
+        height: 40%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+`;
+
+
+
+export default Popular
